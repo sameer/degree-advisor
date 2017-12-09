95f6bd852d7efe70c91b10f610cec28fc559e4e3
Sameer <spuri4096@gmail.com>
2017-12-09 18:05:28 -0500

    Handle the remainder of the spaces + give the actuall error to the user

--- sameerpuri_flaskui.py
+++ sameerpuri_flaskui.py
@@ -42,7 +42,7 @@ def informer():
             course_info = course_desc_dict[form_course]
             course = form_course
         else:
-            error = 'Course not found!'
+            error = 'Course not found: ' + form_course
     return render_template('informer.html', error=error, course=course, course_info=course_info)
 
 
@@ -57,11 +57,11 @@ def recommender():
             str_course_list: List[str] = str_courses.split(';')
             courses: List[cd.Course] = []
             for course_str in str_course_list:
-                course_split: List[str] = course_str.split(' ')
+                course_split: List[str] = course_str.strip().split(' ')
                 courses.append(cd.Course(course_split[0], course_split[1]))
             recommendation_list = spr.recommend_courses_using_liked_courses(courses, num)
         except KeyError as e:
-            error = 'Course not found!'
+            error = 'Course not found: ' + str(e)
     return render_template('recommender.html', error=error, recommendation_list=recommendation_list, course_desc_dict=course_desc_dict)
 
 
@@ -76,13 +76,13 @@ def scheduler():
             initial_state: List[cd.Course] = []
             goal_conditions: List[cd.Course] = []
             for course_str in str_initial_state.split(';'):
-                course_split: List[str] = course_str.split(' ')
+                course_split: List[str] = course_str.strip().split(' ')
                 initial_state.append(cd.Course(course_split[0].strip(), course_split[1].strip()))
             for course_str in str_goal_conditions.split(';'):
-                course_split: List[str] = course_str.split(' ')
+                course_split: List[str] = course_str.strip().split(' ')
                 goal_conditions.append(cd.Course(course_split[0].strip(), course_split[1].strip()))
             result_dict = sps.course_scheduler(course_dict, goal_conditions, initial_state)
             result_plan = list(sorted([(k,v) for k,v in result_dict.items()], key=lambda tuple: tuple[1].terms))
         except KeyError as e:
-            error = 'Course not found!'
+            error = 'Course not found: ' + str(e)
     return render_template('scheduler.html', error=error, result_plan=result_plan, course_desc_dict=course_desc_dict)
